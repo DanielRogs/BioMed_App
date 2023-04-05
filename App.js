1c0f714966d72fcc6896cc64cd6384e24dd11a05
Davi Rodrigues da Rocha <davirocha12.80@gmail.com>
2023-04-05 00:13:48 -0300

    Adicionando a página de login

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import PreHome from './src/Pages/PreHome';
+import LoginPage from './src/Pages/LoginPage';
 
 const Stack = createNativeStackNavigator();
 
@@ -15,6 +16,7 @@ export default function App() {
         }}
       >
         <Stack.Screen name='PreHome' component={PreHome}/>
+        <Stack.Screen name='LoginPage' component={LoginPage}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
