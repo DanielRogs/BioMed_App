cd4f69204e0385bbd78f45e2209c3618758310ad
Daniel Rodrigues <danielrocha.dr142@gmail.com>
2023-04-11 10:48:39 -0300

    PreHome Concluido
    Layout Pronto, faltando wallpaper.

--- src/Pages/PreHome/index.js
+++ src/Pages/PreHome/index.js
@@ -1,15 +1,23 @@
 import React from "react";
 import { View, Text, StyleSheet} from "react-native";
+import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
+
+import PressableB from "../../Components/bluePressable";
+import PressableW from "../../Components/whitePressable";
 
 export default function PreHome({ navigation }) {
     return(
+
         <View style={style.container}>
-            <View style={{marginLeft: -80, marginTop: -600}}>
+            <View style={style.principal}>
                 <Text style={{fontFamily:'ballob', fontSize: 58, marginBottom: -40}}>Bem Vindo</Text>
                 <Text style={{fontFamily:'ballosb', fontSize: 20, color:'#11D876', marginLeft: 4}}>Acompanhe a saúde de sua criança</Text>
             </View>
-            <View>
 
+            <View style={style.espacamento}>
+                <PressableB text="Entrar" onPress={() => navigation.navigate("LoginPage")}></PressableB>
+                <View style={style.espacamentoBotao}/>
+                <PressableW text="Cadastrar-se" onPress={() => navigation.navigate("CadPage")}></PressableW>
             </View>
         </View>
         
@@ -17,6 +25,12 @@ export default function PreHome({ navigation }) {
 }
 
 const style = StyleSheet.create({
+
+    principal: {
+        marginBottom: wp('100%'),
+        marginLeft: hp('-8%')
+    },
+
     container:{
         backgroundColor: '#fffafa',
         flex: 1,
@@ -24,5 +38,12 @@ const style = StyleSheet.create({
         alignItems: 'center'
     },
 
+    espacamento:{
+        marginBottom: wp('-10%')
+    },
+
+    espacamentoBotao:{
+        marginBottom: hp('10%')
+    }
     
 })
