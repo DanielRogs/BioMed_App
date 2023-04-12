c2ea2846006c5f13bf20c1869e2629cd951069b7
Daniel Rodrigues <danielrocha.dr142@gmail.com>
2023-04-12 18:40:56 -0300

    prehome concluida

--- src/Pages/PreHome/index.js
+++ src/Pages/PreHome/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, Text, StyleSheet} from "react-native";
+import { View, Text, StyleSheet, Image} from "react-native";
 import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
 
 import PressableB from "../../Components/bluePressable";
@@ -9,6 +9,10 @@ export default function PreHome({ navigation }) {
     return(
 
         <View style={style.container}>
+            <View style={{justifyContent: "center", alignItems: "center"}}>
+                <Image source={require("../../assets/images/Capa_PreHome.png")} style={style.background}/>
+                <Image source={require("../../assets/images/meninaPulando_PreHome.png")} style={style.background2}/>
+            </View>
             <View style={style.principal}>
                 <Text style={{fontFamily:'ballob', fontSize: 58, marginBottom: -40}}>Bem Vindo</Text>
                 <Text style={{fontFamily:'ballosb', fontSize: 20, color:'#11D876', marginLeft: 4}}>Acompanhe a saúde de sua criança</Text>
@@ -17,7 +21,7 @@ export default function PreHome({ navigation }) {
             <View style={style.espacamento}>
                 <PressableB text="Entrar" onPress={() => navigation.navigate("LoginPage")}></PressableB>
                 <View style={style.espacamentoBotao}/>
-                <PressableW text="Cadastrar-se" onPress={() => navigation.navigate("CadPage")}></PressableW>
+                <PressableW text="Cadastrar-se"></PressableW>
             </View>
         </View>
         
@@ -38,12 +42,29 @@ const style = StyleSheet.create({
         alignItems: 'center'
     },
 
+    background: {
+        position: "absolute",
+        zIndex: 0,
+        width: wp("100%"),
+        height: hp("70%"),
+        top: hp("25%"),
+    },
+
+    background2: {
+        position: "absolute",
+        zIndex: 0,
+        width: wp("70%"),
+        height: hp("35%"),
+        top: hp("22%"),
+        left: wp("-19%")
+    },
+
     espacamento:{
         marginBottom: wp('-10%')
     },
 
     espacamentoBotao:{
         marginBottom: hp('10%')
-    }
+    },
     
 })
